caa5e9968442de3a7a84395578a56be8fcc03576
Glowman554 <glowman434@gmail.com>
2022-03-28 11:59:38 +0200

    some graphics performance fixes

--- features/graphics/sys_copy_to_fb.cpp
+++ features/graphics/sys_copy_to_fb.cpp
@@ -6,7 +6,17 @@
 using namespace syscall;
 
 void syscall::sys_copy_to_fb(interrupts::s_registers* regs) {
-	void* user_address = (void*) regs->rbx;
+	uint32_t* user_address = (uint32_t*) regs->rbx;
 
-	memcpy(renderer::default_framebuffer.base_address, user_address, renderer::default_framebuffer.buffer_size);
+	uint64_t base = (uint64_t) renderer::default_framebuffer.base_address;
+	uint64_t bytes_per_scanline = renderer::default_framebuffer.width * 4;
+	uint64_t fb_height = renderer::default_framebuffer.height;
+	uint64_t fb_size = renderer::default_framebuffer.buffer_size;
+
+	for (int vertical_scanline = 0; vertical_scanline < fb_height; vertical_scanline ++){
+		uint64_t pix_ptr_base = base + (bytes_per_scanline * vertical_scanline);
+		for (uint32_t* pixPtr = (uint32_t*)pix_ptr_base; pixPtr < (uint32_t*)(pix_ptr_base + bytes_per_scanline); pixPtr ++){
+			*pixPtr = user_address[vertical_scanline * renderer::default_framebuffer.width + (pixPtr - (uint32_t*)pix_ptr_base)];
+		}
+	}
 }
