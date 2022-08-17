8dd9a4a56e49a5b160d2bbe281405f3363b630e5
Glowman554 <glowman434@gmail.com>
2022-08-17 19:56:00 +0200

    print program name on sys_dbg call

--- core/syscall/sys_dbg.cpp
+++ core/syscall/sys_dbg.cpp
@@ -2,9 +2,23 @@
 
 #include <utils/log.h>
 
+#include <apic/apic.h>
+#include <scheduler/scheduler.h>
+
 using namespace syscall;
 
 void syscall::sys_dbg(interrupts::s_registers* regs) {
 	char* msg = (char*) regs->rbx;
+
+	if (scheduler::is_scheduler_running) {
+		LAPIC_ID(core_id);
+		scheduler::task_t* task = scheduler::task_queue[core_id]->list[0];
+
+		if (task->argv[0] != nullptr) {
+			debugf_raw("[USER@%s] %s\n", task->argv[0], msg);
+			return;
+		}
+	}
+	
 	debugf_raw("[USER] %s\n", msg);
 }
